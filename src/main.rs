1053b07f0006487c23107cb2ab3a89204583303e
bt7878 <brandontosteis@gmail.com>
2022-03-22 18:33:02 -0400

    use iterators

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,29 @@
 use rand::{thread_rng, Rng};
 
-fn monte_carlo(f: fn(&Vec<f64>) -> f64, a: &[f64], b: &[f64], n: u32) -> Option<f64> {
-    if a.len() != b.len() {
-        return None;
-    }
-
-    let mut w = 1.0;
-
-    for i in 0..a.len() {
-        w *= b[i] - a[i];
-    }
-
-    let mut x_d: Vec<f64> = vec![0.0; a.len()];
-    let mut sum = 0.0;
-
-    for _ in 0..n {
-        for i in 0..x_d.len() {
-            let range = if b[i] >= a[i] {
-                a[i]..=b[i]
-            } else {
-                b[i]..=a[i]
-            };
-            x_d[i] = thread_rng().gen_range(range);
-        }
-        sum += f(&x_d);
-    }
-
-    Some(sum * w / n as f64)
+fn monte_carlo(f: fn(&[f64]) -> f64, a: &[f64], b: &[f64], n: u32) -> f64 {
+    (0..n)
+        .map(|_| {
+            a.iter()
+                .zip(b.iter())
+                .map(|(a_i, b_i)| {
+                    thread_rng().gen_range(if a_i <= b_i { *a_i..=*b_i } else { *b_i..=*a_i })
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|x| f(&x))
+        .sum::<f64>()
+        * a.iter()
+            .zip(b.iter())
+            .map(|(a_i, b_i)| b_i - a_i)
+            .product::<f64>()
+        / n as f64
 }
 
-fn f(x: &Vec<f64>) -> f64 {
+fn f(x: &[f64]) -> f64 {
     x[0] * x[1]
 }
 
 fn main() {
-    let r = monte_carlo(f, &[0.0, 0.0], &[1.0, 2.0], 1000);
-
-    println!("Result: {}", r.unwrap());
+    let r = monte_carlo(f, &[0.0, 0.0], &[1.0, 2.0], 100000);
+    println!("Result: {}", r);
 }
